76da706ed2f8e4a7f70cd78a291feb2d835a338d
Artur Sabanadze <Diakosmisi26@hotmail.com>
2025-03-07 21:26:42 +0100

    Test

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,7 +4,7 @@ import "./Main.css"; // Import the CSS file
 const Main = () => {
   return (
     <div className="main-container">
-      <h1>Here starting your Programming journey</h1>
+      <h1>Heres starting your Programming journey</h1>
       <p>Register your account and start developing your career...</p>
       {/* Add more content here as you build your website */}
     </div>
